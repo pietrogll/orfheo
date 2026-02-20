cdd9b27d184ba383de81fe7775c45cfffa7b9947
pietrogll <pietrogll@aon.com>
2026-02-20 12:05:53 +0100

    Fix admin page rendering

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -5,22 +5,10 @@ module Admin
     before_action :check_admin!
 
     def index
-      success({ message: 'Admin dashboard' })
-    end
-
-    private
-
-    def check_admin!
-      raise Pard::Invalid::Admin unless admin?
-    end
-
-    def admin?
-      return false unless session[:identity]
-
-      # MetaRepos::Admins stores admins by email in the 'email' field
-      # Query by email to check if user is admin
-      admins = MetaRepos::Admins.get(email: session[:identity])
-      admins.present? && admins.any?
+      respond_to do |format|
+        format.html
+        format.json { success({ message: 'Admin dashboard' }) }
+      end
     end
   end
 end
